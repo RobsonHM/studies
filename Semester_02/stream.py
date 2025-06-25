e20f864fc57f7facb69f54eab6a06b2ab0b4b7a4
Robson Henrique <lordcroft@Robsons-MacBook-Air.local>
2025-06-25 15:56:56 +0100

    class 25/06/25

---
+++ Semester_02/stream.py
@@ -0,0 +1,25 @@
+from os import strerror
+
+#try:
+ #   stream = open ("/Users/lordcroft/Documents/stream.txt", "r+")
+  #  content = stream.read()
+   # stream.write("vem quicando, vem sua safada")
+    #print(content)
+    #stream.close()
+
+#except Exception as exec:
+   # print("Cannot open the file", exec)
+
+try:
+    cnt = 0 
+    s = open ("/Users/lordcroft/Documents/stream.txt", "r+")
+    ch = s.read(1)
+    while ch != '':
+        print(ch, end="")
+        cnt +=1
+        ch = s.read(1)
+    s.close()
+    print("\n\nCharacters in file:", cnt)
+except IOError as e:
+    print("I/O error occurred: ", strerror(e.errno))
+
